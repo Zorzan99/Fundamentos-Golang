7534be5707568c9b7744f34576f6b9a9b9772656
Zorzan99 <gabriel_zds@hotmail.com>
2023-11-12 18:02:25 -0300

    Feat: Explicacao  do arquivo estruturas de controle.go

--- 11Estruturas de controle/estruturas-de-controle.go
+++ 11Estruturas de controle/estruturas-de-controle.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Estruturas de controle")
 
+	// ESTRUTURA DE CONTROLE IF-ELSE
 	numero := 10
 
 	if numero > 15 {
@@ -13,11 +14,13 @@ func main() {
 		fmt.Println("Menor ou igual a 15")
 	}
 
+	// ESTRUTURA DE CONTROLE IF-ELSE COM INICIALIZAÇÃO DE VARIÁVEL
 	if outroNumero := numero; outroNumero > 0 {
 		fmt.Println("Número é maior que zero")
 	} else {
-
 		fmt.Println("Número é menor que zero")
 	}
-
 }
+
+//Este código demonstra o uso de estruturas de controle if-else em Go.
+// Os comentários explicam cada parte do código, incluindo a sintaxe do if-else simples e a versão com inicialização de variável antes da condiçã
