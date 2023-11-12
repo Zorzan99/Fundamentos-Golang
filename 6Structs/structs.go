7fa6bbd5d2cab65d94f5350135284b306b7e5407
Zorzan99 <gabriel_zds@hotmail.com>
2023-11-12 17:55:20 -0300

    Feat: Explicacao do arquivo structs.go

--- 6Structs/structs.go
+++ 6Structs/structs.go
@@ -2,32 +2,44 @@ package main
 
 import "fmt"
 
-type usuario struct {
-	nome     string
-	idade    uint8
-	endereco endereco
-}
+// Definição da estrutura "endereco"
 type endereco struct {
 	logradouro string
 	numero     uint8
 }
 
+// Definição da estrutura "usuario" que incorpora a estrutura "endereco"
+type usuario struct {
+	nome     string
+	idade    uint8
+	endereco endereco
+}
+
 func main() {
+	// Impressão de texto informativo
 	fmt.Println("Arquivo structs")
+
+	// Declaração de uma variável do tipo "usuario"
 	var u usuario
 
+	// Atribuição de valores aos campos da estrutura "usuario"
 	u.nome = "Gabriel"
 	u.idade = 24
 
+	// Impressão da estrutura "usuario"
 	fmt.Println(u)
+
+	// Impressão de campos específicos da estrutura "usuario"
 	fmt.Println("Nome", u.nome, "Idade", u.idade)
 
+	// Criação de uma instância da estrutura "endereco" e atribuição a um campo da estrutura "usuario"
 	enderecoExemplo := endereco{"Rua teste", 0}
 
+	// Criação de uma instância da estrutura "usuario" com valores específicos
 	usuario2 := usuario{"Gabriel", 24, enderecoExemplo}
 	fmt.Println(usuario2)
 
+	// Criação de uma instância da estrutura "usuario" com valor específico para o campo "idade"
 	usuario3 := usuario{idade: 24}
 	fmt.Println(usuario3)
-
 }
