cd31a97932e0f2b3210daa404aba126c90801a8f
Zorzan99 <gabriel_zds@hotmail.com>
2023-11-12 17:56:01 -0300

    Feat: Explicacao do arquivo heranca.go

--- 7Heranca/heranca.go
+++ 7Heranca/heranca.go
@@ -2,25 +2,30 @@ package main
 
 import "fmt"
 
+// Definição da estrutura "pessoa"
 type pessoa struct {
 	nome      string
 	sobrenome string
 	idade     uint8
 	altura    uint8
 }
+
+// Definição da estrutura "estudante" que incorpora a estrutura "pessoa"
 type estudante struct {
-	pessoa
+	pessoa    // Incorporação da estrutura "pessoa" na estrutura "estudante"
 	curso     string
 	faculdade string
 }
 
 func main() {
+	// Impressão de texto informativo
 	fmt.Println("Herança")
 
+	// Criação de uma instância da estrutura "pessoa"
 	p1 := pessoa{"João", "Gabriel", 20, 173}
 	fmt.Println(p1)
 
+	// Criação de uma instância da estrutura "estudante" com valores específicos
 	e1 := estudante{p1, "ADS", "unip"}
-	fmt.Println(e1.nome)
-
+	fmt.Println(e1.nome) // Acesso ao campo "nome" da estrutura incorporada "pessoa"
 }
